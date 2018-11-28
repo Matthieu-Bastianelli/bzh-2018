8e1fade06465ea3a3faca644307c638462497738
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-28 10:59:01 +0100

    petit nettoyage du service.js

--- breizhcamp-2018-console/prototype/proto-service.js
+++ breizhcamp-2018-console/prototype/proto-service.js
@@ -1,5 +1,10 @@
 var service = require('../service')
 
+//En asynchrone :
 service.init(function(nb) {
     console.log('[init]', nb, 'sessions trouvées.')
 });
+
+////Ce que ça aurait donné en synchrone (ne fonctionne donc pas puisqu'on est en synchrone)
+//var nbElements =service.init();
+//console.log('....');
--- breizhcamp-2018-console/service.js
+++ breizhcamp-2018-console/service.js
@@ -1,50 +1,34 @@
 // tableau qui contiendra toutes les sessions du BreizhCamp
 var talks = [];
 
+//import de la fonction request.
 var request = require('request');
 
 exports.init = function (callback) {
 
-   //callback(12);
+    // TODO effectuer les requêtes HTTP permettant de récupérer les données du BreizhCamp
+    request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
+
+        talks = talks.concat(body);
+
+        console.log(talks.length);
 
 
-     // TODO effectuer les requêtes HTTP permettant de récupérer les données du BreizhCamp
-     request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function(err, res, body) {
-    if (err) { return console.log('Erreur', err); }
+        // TODO effectuer les requêtes HTTP permettant de récupérer les données du BreizhCamp
+        request('http://2018.breizhcamp.org/json/others.json', { json: true }, function (err, res, body) {
+            if (err) { return console.log('Erreur', err); }
 
-    
-    console.log(body.length);
+            // TODO     => une fois les données récupérées, alimenter la variable talks
+            talks = talks.concat(body);
 
-    talks = talks.concat(body);
-    
-    
-    console.log(talks.length);
-    //});
+            console.log(body.length);
 
-    
-     // TODO effectuer les requêtes HTTP permettant de récupérer les données du BreizhCamp
-     request('http://2018.breizhcamp.org/json/others.json', { json: true }, function(err, res, body) {
-    if (err) { return console.log('Erreur', err); }
-    
-    //console.log(body)
-    talks = talks.concat(body);
-    
-    console.log(body.length);
-    
-    callback(talks.length);
+
+            // TODO         => invoquer la callback avec le nombre de sessions récupérées
+            callback(talks.length);
 
         });
     });
-
-    //var body2 = body;
-    
-    // TODO     => une fois les données récupérées, alimenter la variable talks
-
-    
-
-    //talks=talks.concat(talks);
-
-    // TODO         => invoquer la callback avec le nombre de sessions récupérées
-    //callback(talks.length);
 
 };
