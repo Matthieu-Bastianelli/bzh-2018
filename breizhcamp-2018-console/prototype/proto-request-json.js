e9abe7f5ab094ac1a9d81e3f2e319c30d559c3fc
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-28 10:31:46 +0100

    commit tp2 : requêtes json avec index, ihm et service. Attention rentrer dans la breizh console pour l'appele npm start

--- breizhcamp-2018-console/prototype/proto-request-json.js
+++ breizhcamp-2018-console/prototype/proto-request-json.js
@@ -2,7 +2,10 @@
 // recherche par défaut dans le répertoire node_modules
 var request = require('request')
 
-// Envoie de la requête http
+// Envoie de la requête http :
+//request(url de la requête, 
+//        options : ici forme du corps de la réponse plutôt que chaîne de caractère,
+//        fonction de callback -> exécutée lors du retour de la requête.)
 request('https://jsonplaceholder.typicode.com/posts', { json: true }, function(err, res, body) {
     if (err) { return console.log('Erreur', err); }
 
