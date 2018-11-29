af77e8a320a62552c7e35e2ab4c06b769a65e44e
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-29 17:53:16 +0100

    classe ihm

--- breizhcamp-2018-console/ihm.js
+++ breizhcamp-2018-console/ihm.js
@@ -1,37 +1,33 @@
-const Service = require('./service');
+
 const readline = require('readline');
-//import Service from 'service';
 
-exports.start = () => {
+module.exports = class Ihm{
 
-    let serviceObj = new Service();
-    //let serviceObj = new Service("http://2018.breizhcamp.org/json/talks.json");
+    constructor(service){
+        this.textMenu = "*************************\n1. Rafraichir les données\n2. Lister les sessions\n99. Quitter\nSaisissez une option (1, 2 ou 99): \n";
+        this.rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+        });
+        this.service=service;
+    }
 
-    //let talks=[];
-    const textMenu = "*************************\n1. Rafraichir les données\n2. Lister les sessions\n99. Quitter\nSaisissez une option (1, 2 ou 99): \n";
-
-    const rl = readline.createInterface({
-                    input: process.stdin,
-                    output: process.stdout
-            });
-
-                        
-    questionMenu = () => {
-        rl.question(textMenu, saisie => {
+    start(){
+        this.rl.question(this.textMenu, saisie => {
 
             if(saisie == 99){
                 //On ferme la saisie            
-                rl.close();
+                this.rl.close();
             }else if(saisie==1){
-                serviceObj.init()
+                this.service.init()
                     .then(nb => console.log("[init]", nb, " sessions trouvées.\n... Données mises à jour."))
                     .catch(err => console.log("Erreur de requête", err))
-                    .then(() => questionMenu())
+                    .then(() => this.start())
                 
                     
                 
             }else if(saisie==2){
-                let listeSessions = serviceObj.listerSessions();
+                let listeSessions = this.service.listerSessions();
                 if(listeSessions.length===0){
                     console.log('0 session trouvées. Assurez vous d\'avoir bien rafraîchi les données.')
                 }else{
@@ -43,12 +39,14 @@ exports.start = () => {
                 console.log('Saisie non valide.')
             }
         if(saisie != 99 && saisie != 1){
-            questionMenu();
+            this.start();
         }
                         
         });
     }
-    
-    questionMenu();
 
-};
+}
+    //let talks=[];
+
+    
+
--- breizhcamp-2018-console/index.js
+++ breizhcamp-2018-console/index.js
@@ -1,5 +1,13 @@
-const ihm = require('./ihm');
+const Ihm = require('./ihm');
+const Service = require('./service');
 
 console.log('** Application BreizhCamp 2018 **');
+
+console.log("Création d'une instance de Service...")
+let serviceObj = new Service();
+//let serviceObj = new Service("http://2018.breizhcamp.org/json/talks.json");  // 2ème méthode de création possible.
+
+console.log("Création d'une instance de Ihm...")
+let ihm = new Ihm(serviceObj);
 
 ihm.start();
